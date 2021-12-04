cded15bcee31ce45a692e0da8593c25a4c4b6771
Ayodeji Abodunrin <Ayodeji.abodunrin@wemabank.com>
2021-12-04 04:48:46 +0100

    updated config

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,10 @@ module.exports = function (req, res, next) {
   if (!token) return res.status(401).send("Access denied. No token provided.");
 
   try {
-    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    const decoded = jwt.verify(
+      token,
+      process.env.jwtPrivateKey || config.get("jwtPrivateKey")
+    );
     req.user = decoded;
     next();
   } catch (ex) {
